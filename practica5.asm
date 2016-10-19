8882aca0a31378d09687dc700e66589137baa17a
root <root@jorge-virtual-machine.(none)>
2016-10-19 16:27:02 -0600

    leer entrada del usuario (Path) y escribir un archivo reporte con el contendido del path ingresado

--- practica5.asm
+++ practica5.asm
@@ -2,13 +2,30 @@
 ;200915030
 section .data
 
-file db "./hello.txt", 0
-report db "./report.txt", 0
+file db "hello.txt", 0
+report db "report.res", 0
 msg db "Arqui 1 top! Assembler", 0
+msg2 db "hello.txt", 0
+msg2_size equ $ - msg2
+
+header_txt: db 10,"Universidad de San Carlos de Guatemala"
+                db 10,"Facultad Ingenieria"
+                db 10,"Arquitectura de Computadores y Ensambladores 1"
+                db 10,"Primer Semestre 2016"
+                db 10,"Seccion B"
+                db 10,"Jorge Alberto Rodriguez Mayorga" 
+                db 10,"200915030"
+                db 10,"Practica 5"
+                db 10,""
+                db 10,""
+                db 10,"Ingrese Path al archivo:"
+                db 10,"",10,0
+
 
 len equ 1024
-
+headerlen equ $ - header_txt
 textlen equ $ - msg
+
 
 section .bss 
 
@@ -20,19 +37,50 @@ section .text
 global _start
 
 _start:
+
+    ; Imprimir Header
+    mov eax, 4  
+    mov ebx, 1   
+    mov ecx, header_txt 
+    mov edx, headerlen  
+    int 80h     
+
+    ; Leer input del usuario
+    mov eax, 3
+    mov ebx, 0
+    mov ecx, file
+    mov edx, len
+    int 80h
+
+
+
+    ; Imprimir el texto ingresado por el usuario
+    ;mov eax, 4  ; Instruccion imprimir en consola
+    ;mov ebx, 1  ; 
+    ;mov ecx, msg2 ; Fuente 
+    ;mov edx, msg2_size   ; Tamaño
+    ;int 80h     
+
     
     ; Abrir el archivo
-    mov ebx, file ; nombre del archivo
     mov eax, 5  ; Instruccion para abrir
+    mov ebx, msg2 ; nombre del archivo
     mov ecx, 0  ; 
     int 80h     
 
     ; Guardare el contenido en el buffer
     mov eax, 3  
     mov ebx, eax
-    mov ecx, buffer 
-    mov edx, len    
+    mov ecx, buffer
+    mov edx, len  
     int 80h     
+
+    ; Imprimir el texto del buffer
+    mov eax, 4  ; Instruccion imprimir en consola
+    mov ebx, 1  ; 
+    mov ecx, buffer ; Fuente (buffer) 
+    mov edx, len   ; Tamaño
+    int 80h  
 
     ; Crear el archivo reporte
     mov eax, 8
@@ -41,21 +89,13 @@ _start:
     mov edx, len
     int 80h
 
-    ; Escribir en el reporte
-    mov ebx, eax
-    mov eax, 4
-    mov ecx, msg
-    mov edx, textlen
-    int 80h
-    mov eax, 6
-    int 80h
 
-    ; Imprimir el texto del buffer
-    mov eax, 4  ; Instruccion imprimir en consola
-    mov ebx, 1  ; 
-    mov ecx, buffer ; Fuente (buffer) 
-    mov edx, len    ; Tamaño
-    int 80h     
+    ; Escribir en el reporte
+    mov eax, 4
+    mov ebx, eax
+    mov edx, len
+    mov ecx, buffer
+    int 80h
 
     ; Cerrar el archivo
     mov eax, 6  
