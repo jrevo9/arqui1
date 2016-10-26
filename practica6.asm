b6fe362d7e43559c16eceb5f73c92fce96304e13
root <root@jorge-virtual-machine.(none)>
2016-10-26 11:44:46 -0600

    Menu de navegacion funcionando al 100%

--- practica6.asm
+++ practica6.asm
@@ -2,10 +2,7 @@
 ;200915030
 section .data
 
-calc_header db "Modo Calculadora activado", 0
-
-
-header_txt: db 10,"Universidad de San Carlos de Guatemala"
+header_txt db 10,"Universidad de San Carlos de Guatemala"
                 db 10,"Facultad Ingenieria"
                 db 10,"Arquitectura de Computadores y Ensambladores 1"
                 db 10,"Primer Semestre 2016"
@@ -18,74 +15,101 @@ header_txt: db 10,"Universidad de San Carlos de Guatemala"
                 db 10,"Que desea realizar? 1. Calculadora 0. Salir"
                 db 10,"",10,0
 
-
-len equ 100
 headerlen equ $ - header_txt
+;
+calc_header db "Modo Calculadora activado",10,13,
 calclen equ $ - calc_header
 
 section .bss 
 
-buffer: resb 100
-lpName:  resb 11  
-     
-name_len equ    $ - lpName
+menuInput:  resb 1 
+calcInput:  resb 2 
 
 
 section .text
 
+Next:
+    cmp al, '0'
+    je Exit
+    jmp Home
+
 global _start
 
+%macro print 2
+        mov eax, 4  
+        mov ebx, 1   
+        mov ecx, %1  
+        mov edx, %2  
+        int 80h  
+
+%endmacro
+
 _start:
+    mov ah, 00
+    mov al, 03h
+    int 80h
+    jmp Home
+    call Exit
+
+
+Home:
 
     ; Imprimir Header
-    mov ecx, header_txt 
-    mov edx, headerlen  
-    call PrintConsole   
+    ;mov eax, 4  ; Instruccion imprimir en consola
+    ;mov ebx, 1 
+    ;mov ecx, header_txt 
+    ;mov edx, headerlen  
+    ;int 80h
+
+    print header_txt, headerlen
 
     ; Leer input del usuario
-    mov ecx, lpName
-    mov edx, name_len
-    call ReadText
+    mov eax, 3 
+    mov ebx, 0
+    mov ecx, menuInput
+    mov edx, 1
+    int 80h 
 
-    ;Imprimir el texto ingresado por el usuario
-    mov ecx, lpName ; Fuente 
-    mov edx, name_len  ; Tamaño
-    call PrintConsole
+    mov al,  byte [menuInput]
 
-    cmp ecx,1
+    cmp al,'1'
     je CalculatorMode
-    jmp _start
-
-    cmp ecx,0
-    je Exit
+    jmp Next
+    ret
 
 CalculatorMode:
+    
 
+    ; Imprimir Instruccion
+    mov eax, 4  ; Instruccion imprimir en consola
+    mov ebx, 1 
     mov ecx, calc_header ; Fuente 
     mov edx, calclen  ; Tamaño
-    call PrintConsole
+    int 80h  
 
+    ; Leer input del usuario 
+    mov eax, 3 ; Instruccion para leer de consola
+    mov ebx, 0 
+    mov ecx, calcInput ; Destino
+    mov edx, 2 ; Tamaño
+    int 80h 
 
- Exit:  
+    mov ah, 00
+    mov al, 03h
+    int 80h
+
+    ;Imprimir el texto ingresado por el usuario
+    mov eax, 4  ; Instruccion imprimir en consola
+    mov ebx, 1 
+    mov ecx, calcInput ; Fuente 
+    mov edx, 2 ; Tamaño
+    int 80h 
+
+    call Home
+
+Exit:  
+ 
     ; Instrucciones para cerrar el programa
     mov eax, 1  
     mov ebx, 0 
     int 80h
-
-ReadText:
-    mov     ebx, 0 
-    mov     eax, 3     
-    int     80h 
-    ret  
-
-PrintConsole:
-    mov eax, 4  ; Instruccion imprimir en consola
-    mov ebx, 1  
-    int 80h     
-    ret
-
-FreeMem:
-    mov ebx, 1
-    mov eax, 45
-    int 80h
-    ret
