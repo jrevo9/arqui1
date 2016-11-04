e8fa8229a318bf42bcdc2d66af5849e7d051f882
root <root@jorge-virtual-machine.(none)>
2016-11-04 03:56:48 -0600

    Ingreso e impresión de funcion, inicio de derivada

--- Proyecto2.asm
+++ Proyecto2.asm
@@ -30,25 +30,48 @@ error2 db "ERROR!! Contraseña incorrecta.",10,13
 error2_len equ $ - error2
 error3 db "Error!! La contraseña solo pueden ser numeros.",10,13
 error3_len equ $ - error3
-error4 db "Error! El usuario ya existe.",10,13
+error4 db "Error!! El usuario ya existe.",10,13
 error4_len equ $ - error4
-error5 db "validating password syntax",10,13
-error5_len equ $ - error5
+error5 db "Error!! No se ha ingresado funcion.",10,13
+error5_len: equ $-error5
 
 alert1 db "Sesión Iniciada Exitosamente!",10,13
 alert1_len equ $ - alert1
 alert2 db "Usuario Creado con Exito!",10,13
 alert2_len equ $ - alert2
 
+msg1:  db "Coeficiente  ",0
+msg1_len: equ $-msg1
+
+x4_txt: db "xˆ4 ",0 
+x3_txt: db "xˆ3 ",0 
+x2_txt: db "xˆ2 ",0 
+x1_txt: db "x ",0
+x_txt: db "C ",0
+
 tmpUser db ""
 comma : db 2ch
 dotcom : db 3bh
-
+plus : db 2Bh
+space : db 20h
+equal : db 3Dh
+nln: db  0xA
+star: db 2Ah
+dash: db 2Dh
+slash: db 2Fh
+dot: db 2Eh
+_eof: db 0x0
+_op: db 2Bh
+sx4 db 2Bh
+sx3 db 2Bh
+sx2 db 2Bh
+sx1 db 2Bh
+sx0 db 2Bh
 
 
 section .bss 
 
-userInput resb 2
+userInput resb 32
 buffer resb 100
 user resb 6
 pass resb 6
@@ -56,6 +79,13 @@ buf resb 1
 us resb 1
 pas resb 1
 login resb 1 ; 0 login 1 register
+vx0 resb 4
+vx3 resb 4
+vx2 resb 4
+vx1 resb 4
+vx4 resb 4
+buff1 resb 100
+tmp resb 1
 
 
 
@@ -130,7 +160,6 @@ _login:
     mov bl, 0
     mov [login], bl
     call _validateUser
-    ;call _validatePass
 
 ret
 
@@ -430,18 +459,20 @@ ret
 
 _finish_validation:
     cmp dl, 1
-    jz _secondary_menu
+    jz _proceed
     jmp _login
 
 _wrong_pass:
     mov ecx, 0
-    mov eax, 0
-    mov dl, 0 
     print error2, error2_len
     jmp _login
 
-_secondary_menu:
+_proceed:
     print alert1, alert1_len 
+    call _secondary_menu
+
+_secondary_menu:
+   
     print secmenu, secmenu_len
     
     ; Leer input del usuario
@@ -449,11 +480,11 @@ _secondary_menu:
 
     mov dl, byte [userInput]
     cmp dl, '1'
-    ;jz  _login
+    jz  _get_function
     cmp dl, '2'
-    ;jz  _register
+    jz  _function_requested
     cmp dl, '3'
-    jz  _login
+    ;jz  _login
     cmp dl, '4'
     ;jz  _register
     cmp dl, '5'
@@ -463,7 +494,294 @@ _secondary_menu:
 
     call _secondary_menu
 
+_get_function:
+       
+    call _print_msg1 
+    print x1_txt, 3
+    call print_space
+    call _read_input_number
+    mov [vx1], eax
+    xor ebx, ebx
+    mov bl , byte [_op]
+    mov [sx1], bl
 
+    xor eax, eax
+    xor ebx,ebx
+    call _print_msg1 
+    print x2_txt, 4
+    call print_space
+    xor eax, eax
+    xor ebx,ebx
+    xor edx,edx
+    xor ecx, ecx
+    call _read_input_number
+    mov [vx2], eax
+    xor ebx, ebx
+    mov bl , byte [_op]
+    mov [sx2], bl
+
+    call _print_msg1 
+    print x3_txt, 4
+    call print_space
+    xor eax, eax
+    xor ebx,ebx
+    xor edx,edx
+    xor ecx, ecx
+    call _read_input_number
+    mov [vx3], eax
+    xor ebx, ebx
+    mov bl , byte [_op]
+    mov [sx3], bl
+
+    xor eax, eax
+    xor ebx,ebx
+    call _print_msg1 
+    print x4_txt, 4
+    call print_space
+    call _read_input_number
+    mov [vx4], eax
+    xor ebx, ebx
+    mov bl , byte [_op]
+    mov [sx4], bl
+
+    call _print_msg1 
+    print x_txt, 3
+    call print_space
+    xor eax, eax
+    call _read_input_number
+    mov [vx0], eax
+    xor ebx, ebx
+    mov bl , byte [_op]
+    mov [sx0], bl
+    xor ebx, ebx
+    mov bl, 0x1
+    mov [_eof],bl
+
+ret
+
+_print_function:
+    
+    mov eax, [vx1]
+    cmp eax, 0x0
+    jz write_x0
+    xor ebx,ebx
+    mov bl, byte [sx1]
+    cmp bl, [dash]
+    jnz write_x1
+    push eax
+    print sx1,1
+    pop eax
+
+    write_x1:
+    call _print_number
+    print x1_txt,3
+    call print_space
+
+    write_x2:   
+    mov eax, [vx2]
+    cmp eax, 0x0
+    jz write_x1
+    push eax
+    print sx2,1
+    pop eax
+    call _print_number
+    print x2_txt,4
+    call print_space
+
+    write_x3:
+    mov eax, [vx3]
+    cmp eax, 0x0
+    jz write_x2
+    push eax
+    print sx3,1
+    pop eax
+    call _print_number
+    print x3_txt,4
+    call print_space
+
+    write_x4:
+    mov eax, [vx4]
+    cmp eax, 0x0
+    jz  write_x3
+    push eax
+    print sx4,1
+    pop eax
+    call _print_number
+    print x4_txt,4
+    call print_space
+
+    write_x0:
+    mov eax, [vx0]
+    cmp eax, 0x0
+    jz end_print
+    push eax
+    print sx0,1
+    pop eax
+    call _print_number
+
+end_print:
+    call _print_ln
+
+jmp _secondary_menu
+
+
+_function_requested:
+    xor ecx, ecx
+    mov cl, byte [_eof]
+    cmp cl, 0x0
+    jz _no_function_entered
+    jmp _print_function
+
+
+_derivate_requested:
+    xor ecx, ecx
+    mov cl, byte [_eof]
+    cmp cl, 0x0
+    jz _no_function_entered
+    jmp _print_function
+
+     
+
+_no_function_entered:
+    print error5, error5_len
+    jmp _secondary_menu
+
+_print_number:
+    push edx
+    push ecx
+    push ebx
+    push eax
+
+    cmp eax, 0x0
+
+    jz zero
+
+    xor  edx, edx
+    xor  ecx, ecx
+    mov  ecx, 0xA
+    div ecx
+    push edx
+    call _print_number
+    pop edx
+    add edx, 0x30
+    mov [userInput], edx
+    call _print_userInput
+zero:       
+    pop eax
+    pop ebx
+    pop ecx
+    pop edx
+ret
+
+_print_userInput:
+    push edx
+    push ecx
+    push ebx
+    push eax
+
+    print userInput, 1
+
+    pop eax
+    pop ebx
+    pop ecx
+    pop edx
+ret
+
+_print_ln:
+    push edx
+    push ecx
+    push ebx
+    push eax
+    print nln,0x1
+    pop eax
+    pop ebx
+    pop ecx
+    pop edx
+ret
+
+
+print_space:   
+
+    push edx
+    push ecx
+    push ebx
+    push eax
+
+    print space, 1
+
+    pop eax
+    pop ebx
+    pop ecx
+    pop edx
+ret
+
+
+_print_msg1: 
+    push edx
+    push ecx
+    push ebx
+    push eax
+    
+    print msg1, msg1_len
+
+    pop eax
+    pop ebx
+    pop ecx
+    pop edx
+ 
+ret
+
+_read_number:
+    cmp ecx,edx         
+    jz end_read
+    push edx        
+    xor edx, edx    
+    xor bl, bl      
+    mov edx, 0xA    
+    mov bl, [buff1 + ecx]   
+    mov [tmp], bl       
+    push ecx            
+    mov ecx, dword [tmp] 
+    sub ecx, 0x30       
+    mul edx             
+    add eax, ecx        
+    pop ecx             
+    pop edx             
+    add ecx, 0x1        
+    call _read_number 
+end_read:
+ret
+
+_read_input_number:
+    push edx  
+    push ecx  
+    mov  byte [_op], 2Bh
+    read buff1,100
+    mov edx, eax    
+    
+    mov edx, eax    
+    sub edx, 0x1     
+
+    xor ecx, ecx    
+    mov al, [buff1] 
+    cmp al, [dash]
+    jnz  positive
+    mov [_op], al
+    add ecx, 0x1
+    jmp continue
+
+positive:
+    cmp al, [plus]
+    jnz continue
+    add ecx,0x1
+
+continue:
+    xor eax, eax     ; clean eax
+    call _read_number
+
+    pop ecx
+    pop edx
+ret
 
 _exit:  
     mov eax, 1  
